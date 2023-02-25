125dba3f41f525e7bd4eddaa6c7d432042a497d9
Mateo <mateo2823@hotmail.com>
2023-02-25 03:56:33 -0400

    desktop

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const main = document.getElementById('main');
-const chevron = document.getElementsByClassName('gg-chevron-down')[0];
-
-function abrirCard(){
-    main.style.color = 'black';
-}
-
-chevron.addEventListener(onclick, abrirCard);
